9b652ea8317aa38914d273387659847f60bb37ef
T.Maikon <thomasmaikon@hotmail.com>
2021-11-18 23:43:39 -0300

    Iniciando Projeto

---
+++ BasicAI/BlackHole.cpp
@@ -0,0 +1,139 @@
+/**********************************************************************************
+// Orange (Código Fonte)
+//
+// Criação:     05 Ago 2019
+// Atualização: 11 Nov 2021
+// Compilador:  Visual C++ 2019
+//
+// Descrição:   Objeto faz movimento retilíneo
+//
+**********************************************************************************/
+
+#include "BasicAI.h"
+#include "BlackHole.h"
+#include "Random.h" 
+#include "Image.h"
+#include "Missile.h"
+// ---------------------------------------------------------------------------------
+
+BlackHole::BlackHole(float pX, float pY)
+{
+    sprite = new Sprite("Resources/ResourcesUnidade3/buracoNegro.png");
+    BBox(new Circle(50.0f));
+    MoveTo(pX, pY);
+    type = Ids::BLACKHOLE;
+    forca = 30.0f;
+    /* sprite = new Sprite(BasicAI::orange);
+
+     // ajusta o vetor velocidade
+     speed.RotateTo(ang);
+     speed.ScaleTo(400);
+     RotateTo(-speed.Angle());
+     BBox(new Circle(20.0f));
+     type = ORANGE;
+
+     // configuração do emissor de partículas
+     Generator emitter;
+     emitter.imgFile = "Resources/Spark.png";    // arquivo de imagem
+     emitter.angle = speed.Angle() + 180;        // ângulo base do emissor
+     emitter.spread = 5;                         // espalhamento em graus
+     emitter.lifetime = 0.4f;                    // tempo de vida em segundos
+     emitter.frequency = 0.010f;                 // tempo entre geração de novas partículas
+     emitter.percentToDim = 0.8f;                // desaparece após 60% da vida
+     emitter.minSpeed = 100.0f;                  // velocidade mínima das partículas
+     emitter.maxSpeed = 200.0f;                  // velocidade máxima das partículas
+     emitter.color.r = 1.0f;                     // componente Red da partícula
+     emitter.color.g = 0.5;                      // componente Green da partícula
+     emitter.color.b = 0.0f;                     // componente Blue da partícula
+     emitter.color.a = 1.0f;                     // transparência da partícula
+
+     // cria sistema de partículas
+     tail = new Particles(emitter);
+     tailCount = 0;
+
+     // incrementa contagem
+     ++Hud::oranges;*/
+}
+
+// ---------------------------------------------------------------------------------
+
+BlackHole::~BlackHole()
+{
+    /*delete sprite;
+    delete tail;
+
+    // decrementa contagem
+    Hud::particles -= tailCount;
+    --Hud::oranges;*/
+}
+
+// -------------------------------------------------------------------------------
+
+void BlackHole::OnCollision(Object* obj)
+{
+    
+    switch(obj->Type())
+    {
+    
+    case Ids::MISSILE: {
+
+        auto* missile = (Missile*)obj;
+        missile->Rotate(20);
+        missile->ScaleTo(missile->Scale() - 5);
+        break;
+    }
+   
+    case Ids::PLAYER: {
+        auto* player = (Player*)obj;
+        player->MoveTo(300, 300);
+        //player->ScaleTo(player->Scale() - 5);
+        break;
+    }
+    
+    }
+    
+    /*if (obj->Type() == MISSILE)
+    {
+        BasicAI::scene->Delete(obj, STATIC);
+        BasicAI::scene->Delete(this, MOVING);
+        BasicAI::scene->Add(new Explosion(x, y), STATIC);
+        BasicAI::audio->Play(EXPLODE);
+    }*/
+}
+
+// -------------------------------------------------------------------------------
+
+void BlackHole::Update()
+{
+    Rotate(10);
+    /*// movimenta objeto pelo seu vetor velocidade
+    Translate(speed.XComponent() * gameTime, -speed.YComponent() * gameTime);
+
+    // ajusta ângulo do vetor na direção oposta
+    if (x < 50 || y < 50 || x > game->Width() - 50 || y > game->Height() - 50)
+    {
+        Rotate(180);
+        speed.Rotate(180);
+        Translate(speed.XComponent() * gameTime, -speed.YComponent() * gameTime);
+    }
+
+    // atualiza calda da nave
+    tail->Config().angle = speed.Angle();
+    tail->Generate(x - 10 * cos(speed.Radians()), y + 10 * sin(speed.Radians()));
+    tail->Update(gameTime);
+
+    Hud::particles -= tailCount;
+    tailCount = tail->Size();
+    Hud::particles += tailCount;*/
+}
+
+// ---------------------------------------------------------------------------------
+
+void BlackHole::Draw()
+{
+    sprite->Draw(x, y, Layer::MIDDLE, scale, rotation);
+    /*sprite->Draw(x, y, Layer::LOWER, scale, rotation);
+    tail->Draw(Layer::LOWER, 1.0f);*/
+}
+
+// -------------------------------------------------------------------------------
---
+++ BasicAI/Boss.cpp
@@ -0,0 +1,136 @@
+#include "Boss.h"
+#include "BasicAI.h"
+#include "Player.h"
+
+// -------------------------------------------------------------------------------
+Boss::Boss()
+{
+    // inicializa controle
+//    gamepad = new Controller();
+//    gamepadOn = gamepad->Initialize();
+
+    // configuração do objeto
+    sprite = new Sprite("Resources/Player.png");
+    //missile = new Image("Resources/Missile.png");
+    speed.RotateTo(90.0f);
+    speed.ScaleTo(0.0f);
+    BBox(new Circle(18.0f));
+    MoveTo(game->CenterX()+100, game->CenterY()+100);
+    type = Ids::BOSS;
+
+    // diparo habilitado
+    firingAngle = 0.0f;
+    keysPressed = false;
+    axisCtrl = true;
+    keysCtrl = true;
+    start = 0;
+    timer.Start();
+}
+
+// -------------------------------------------------------------------------------
+
+Boss::~Boss()
+{
+    delete sprite;
+    //delete missile;
+}
+
+// -------------------------------------------------------------------------------
+
+bool Boss::KeysTimed(bool pressed, float time)
+{
+    // se já passou o tempo para o próximo disparo
+    if (keysCtrl)
+    {
+        // se há qualquer seta pressionada
+        if (pressed)
+        {
+            keysCtrl = false;
+            start = timer.Stamp();
+            return true;
+        }
+    }
+    // senão aguarda o momento certo
+    else if (timer.Elapsed(start, time))
+    {
+        keysCtrl = true;
+    }
+
+    // teclas não pressionadas ou tempo não atingido
+    return false;
+}
+
+// -------------------------------------------------------------------------------
+
+bool Boss::AxisTimed(int axisX, int axisY, float time)
+{
+    // se já passou o tempo para o próximo disparo
+    /*if (axisCtrl)
+    {
+        // a magnitude é a distância do eixo para o seu centro
+        float magnitude = Point::Distance(Point(0, 0), Point(float(gamepad->Axis(axisX)), float(gamepad->Axis(axisY))));
+
+        // se há qualquer movimento no eixo
+        if (magnitude > 0)
+        {
+            axisCtrl = false;
+            start = timer.Stamp();
+            return true;
+        }
+    }
+    // senão aguarda o momento certo para testar
+    else if (timer.Elapsed(start, time))
+    {
+        axisCtrl = true;
+    }
+
+    // eixo não acionado ou tempo não atingido*/
+    return false;
+}
+
+// -------------------------------------------------------------------------------
+
+void Boss::Move(Vector&& v)
+{
+    // soma vetor movimento (v) ao vetor velocidade
+    speed.Add(v);
+
+    // limita velocidade máxima
+    if (speed.Magnitude() > 10.0f)
+        speed.ScaleTo(10.0f);
+}
+
+// -------------------------------------------------------------------------------
+
+void Boss::Update()
+{
+    // ajusta ângulo do vetor
+    float angle = Line::Angle(Point(x, y), Point(BasicAI::player->X(), BasicAI::player->Y()));
+    speed.RotateTo(angle);
+   
+    Move(Vector(angle, 10 * gameTime));
+
+    // movimenta objeto pelo seu vetor velocidade
+    float spaceBetweenShips = Point::Distance(Point(x, y), Point(BasicAI::player->X(), BasicAI::player->Y()));
+    if (spaceBetweenShips >= 500) {
+        Translate(speed.XComponent() * 50.0f * gameTime, -speed.YComponent() * 50.0f * gameTime);
+    }else if(Point::Distance(Point(x, y), Point(BasicAI::player->X(), BasicAI::player->Y())) < 400){
+        speed.RotateTo(angle);
+        speed.ScaleTo(-300.0f * gameTime);
+        Translate(speed.XComponent() * 100.0f * gameTime, -speed.YComponent() * 100.0f * gameTime);
+    }
+
+}
+
+// ---------------------------------------------------------------------------------
+
+void Boss::Draw()
+{
+    sprite->Draw(x, y, Layer::MIDDLE, 1.0f, -speed.Angle() + 90.0f);
+}
+
+void Boss::OnCollision(Object* obj)
+{
+}
+
+// -------------------------------------------------------------------------------
